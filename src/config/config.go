22030d88e0850cc0a10f3d1b52361a00ca77c9aa
ruairigibney <ruairigibney@gmail.com>
2021-04-20 19:07:39 +0100

    Restructure go code (#5)
    * Restructure golang code

---
+++ src/config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"github.com/gorilla/sessions"
+	"github.com/patrickmn/go-cache"
+)
+
+type Config struct {
+	LogDirectory     string `envconfig:"LOG_DIRECTORY"`
+	ArchiveDirectory string `envconfig:"ARCHIVE_DIRECTORY"`
+	PasscodePort     string `envconfig:"PASSCODE_PORT"`
+	ArchivePort      string `envconfig:"ARCHIVE_PORT"`
+	Secret           string `envconfig:"SECRET"`
+	Host             string `envconfig:"HOST"`
+	Store            *sessions.CookieStore
+	C                *cache.Cache
+}
