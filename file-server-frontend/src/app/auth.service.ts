9fdb64086f4ab5a5b9794ac8b52b1c67477329e5
Ruairi Gibney <ruairigibney@gmail.com>
2021-04-20 23:04:17 +0100

    Add Angular frontend

---
+++ file-server-frontend/src/app/auth.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  public gotCookie: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  constructor(private router: Router, private http: HttpClient, private route: ActivatedRoute,
+    private cookieService: CookieService) { }
+
+  doAuth(): void {
+    if (this.cookieService.check('wolfpack-file-server')) {
+      this.gotCookie.next(true);
+      return; 
+    }
+
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map(() => this.rootRoute(this.route)),
+      filter((route: ActivatedRoute) => route.outlet === 'primary'),
+    ).subscribe((route: ActivatedRoute) => {
+      var passcode = route.snapshot.queryParamMap.get('passcode');
+      if (passcode) {
+        this.http.get(`${environment.apiUrl}/token?passcode=${passcode}`, {withCredentials: true}).subscribe(
+          () => {
+            this.gotCookie.next(true);
+            this.router.navigate(['/incidents']);
+          }
+        )
+      }
+    });
+  }
+
+  private rootRoute(route: ActivatedRoute): ActivatedRoute {
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+    return route;
+  }
+
+}
--- src/http/http.go
+++ src/http/http.go
@@ -2,19 +2,18 @@ package http
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/sessions"
 	"github.com/patrickmn/go-cache"
+	"github.com/rs/cors"
 	"github.com/ruairigibney/wolfpack-file-server/src/auth"
 	"github.com/ruairigibney/wolfpack-file-server/src/config"
 )
-
-type cookieStore struct {
-	*sessions.CookieStore
-}
 
 type getPassCodeRequestBody struct {
 	Secret string
@@ -24,20 +23,28 @@ type Cfg struct {
 	*config.Config
 }
 
+type FileDetails struct {
+	FileName string
+}
+
 func HttpHandler(c *config.Config) {
+	crs := cors.AllowAll()
+
 	cfg := Cfg{c}
 	archiveMux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir(c.ArchiveDirectory))
 	archiveMux.Handle("/", cfg.RestrictedHandler((fileServer)))
 	log.Printf("Serving archive %s on HTTP port: %v\n", c.ArchiveDirectory, c.ArchivePort)
+	handler := cors.Default().Handler(archiveMux)
+
 	go func() {
-		log.Fatal(http.ListenAndServe(":"+c.ArchivePort, archiveMux))
+		log.Fatal(http.ListenAndServe(":"+c.ArchivePort, crs.Handler(handler)))
 	}()
 
 	passCodeMux := http.NewServeMux()
 	passCodeMux.HandleFunc("/passcode", cfg.GetPassCode)
 	log.Printf("Serving passcodes on HTTP port: %v\n", c.PasscodePort)
-	log.Fatal(http.ListenAndServe(":"+c.PasscodePort, passCodeMux))
+	log.Fatal(http.ListenAndServe(":"+c.PasscodePort, crs.Handler((passCodeMux))))
 }
 
 func (c *Cfg) GetPassCode(resp http.ResponseWriter, req *http.Request) {
@@ -63,39 +70,127 @@ func (c *Cfg) GetPassCode(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 	c.C.Add(passcode, nil, cache.DefaultExpiration)
-	url := fmt.Sprintf("%s:%s/?passcode=%s", c.Host, c.ArchivePort, passcode)
+	var url string
+	if c.IsDev {
+		url = fmt.Sprintf("%s:4200?passcode=%s", c.Host, passcode)
+	} else {
+		url = fmt.Sprintf("%s?passcode=%s", c.Host, passcode)
+	}
 	resp.Write([]byte(url))
-	return
 }
 
 func (c *Cfg) RestrictedHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		resp = setHeaders(resp, c.Host, c.IsDev)
+
 		session, _ := c.Store.Get(req, "wolfpack-file-server")
 		if session.Values["token"] == nil {
-			passcodeQueryParams := req.URL.Query()["passcode"]
-			if len(passcodeQueryParams) == 0 {
-				http.Error(resp, "No passcode", http.StatusForbidden)
+			if req.URL.Path != "/api/token" {
+				http.Error(resp, "Not authenticated", http.StatusForbidden)
 				return
+			}
+			status, err := c.getToken(*session, resp, req)
+			if err != nil {
+				http.Error(resp, err.Error(), status)
 			}
 
-			if _, found := c.C.Get(passcodeQueryParams[0]); !found {
-				http.Error(resp, "Invalid OTP", http.StatusInternalServerError)
-				return
-			} else {
-				var err error
-				session.Values["token"], err = auth.GenerateSecureKey(32)
-				if err != nil {
-					http.Error(resp, "Error generating token", http.StatusInternalServerError)
-					return
-				}
-				err = session.Save(req, resp)
-				if err != nil {
-					http.Error(resp, err.Error(), http.StatusInternalServerError)
-					return
-				}
-			}
+			resp.WriteHeader(status)
+			return
 		}
 
-		h.ServeHTTP(resp, req)
+		switch req.URL.Path {
+		case "/api/files/list":
+			c.listFiles(resp, req)
+		case "/api/files/content":
+			c.getFile(resp, req)
+		}
+
 	})
+}
+
+func (c *Cfg) getToken(session sessions.Session, resp http.ResponseWriter, req *http.Request) (int, error) {
+	passcodeQueryParams := req.URL.Query()["passcode"]
+	if len(passcodeQueryParams) == 0 {
+		return http.StatusForbidden, errors.New("no passcode")
+	}
+
+	if _, found := c.C.Get(passcodeQueryParams[0]); !found {
+		return http.StatusInternalServerError, errors.New("invalid OTP")
+	} else {
+		var err error
+		session.Values["token"], err = auth.GenerateSecureKey(32)
+		if err != nil {
+			return http.StatusInternalServerError, errors.New("error generating token")
+		}
+		err = session.Save(req, resp)
+		if err != nil {
+			return http.StatusInternalServerError, errors.New("error saving session")
+		}
+		c.C.Delete(passcodeQueryParams[0])
+	}
+
+	return http.StatusOK, nil
+}
+
+func (c *Cfg) getFile(resp http.ResponseWriter, req *http.Request) {
+	filenameQueryParams := req.URL.Query()["filename"]
+	if len(filenameQueryParams) == 0 {
+		http.Error(resp, "No file specified", http.StatusBadRequest)
+		return
+	}
+
+	file, err := ioutil.ReadFile(fmt.Sprintf("%s/%s", c.ArchiveDirectory, filenameQueryParams[0]))
+	if err != nil {
+		http.Error(resp, "Error reading file", http.StatusInternalServerError)
+		return
+	}
+
+	resp.Header().Set("Content-Type", "text/html")
+	resp.WriteHeader(http.StatusOK)
+	resp.Write(file)
+}
+
+func setHeaders(resp http.ResponseWriter, host string, isDev bool) http.ResponseWriter {
+	if isDev {
+		resp.Header().Set("Access-Control-Allow-Origin", fmt.Sprintf("%s:4200", host))
+	} else {
+		resp.Header().Set("Access-Control-Allow-Origin", host)
+	}
+	resp.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+	resp.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	resp.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	return resp
+}
+
+func (c *Cfg) listFiles(resp http.ResponseWriter, req *http.Request) {
+	files, err := ioutil.ReadDir(c.ArchiveDirectory)
+	if err != nil {
+		http.Error(resp, "Error reading dir", http.StatusInternalServerError)
+		return
+	}
+
+	var fD []FileDetails
+
+	for _, v := range files {
+		if v.Name() != "" {
+
+			file := FileDetails{
+				FileName: v.Name(),
+			}
+
+			fD = append(fD, file)
+		}
+
+	}
+
+	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteHeader(http.StatusOK)
+	body, err := json.Marshal(fD)
+	if err != nil {
+		http.Error(resp, "Error marshalling json", http.StatusInternalServerError)
+		return
+	}
+
+	resp.Write(body)
 }
