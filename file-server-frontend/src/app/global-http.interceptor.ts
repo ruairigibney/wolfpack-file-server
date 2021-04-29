6ff252659d807d3b27219c22c7e166816b422bb7
ruairigibney <ruairigibney@gmail.com>
2021-04-29 21:30:52 +0100

    Handle 403 & revert sort to name sort (#10)

---
+++ file-server-frontend/src/app/global-http.interceptor.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+  HttpResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class GlobalHttpInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    console.log(request);
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 403) {
+          this.authService.removeCookie();
+        }
+
+        return throwError(error);
+      })
+    )
+  }
+}
